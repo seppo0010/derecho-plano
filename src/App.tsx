6e0e9762cd9cbc6bcf1c886c68a3e7a1524f46aa
Sebastian Waisbrot <seppo0010@gmail.com>
2022-08-18 11:51:17 -0300

    Map data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './App.css';
+import { floors } from './Data';
 
 function App() {
   return (
     <div>
+      {Object.entries(floors).map(([name, rooms]) => (<div className="floor" key={name}>
+        <img src={`${process.env.PUBLIC_URL}/images/${encodeURIComponent(name)}.png`} alt="" />
+        {rooms.map(({ name, top, left, width, height }) => (<div key={name} className="room" style={{
+            top,
+            left,
+            width,
+            height,
+          }}>
+        </div>))}
+      </div>))}
     </div>
   );
 }
